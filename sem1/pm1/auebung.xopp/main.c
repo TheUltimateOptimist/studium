b649d749c0719e967f69a98c7f19208f53ab5ea8
TheUltimateOptimist <digitalConfidence@web.de>
2023-11-08 07:44:42 +0100

    first commit

---
+++ sem1/pm1/auebung.xopp/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int A = a * b;
+    int U = 2 * (a + b);
+    printf("Fuer a = %d und b = %d ist A = %d und U = %d", a, b, A, U);
+}
